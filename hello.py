4dddfc086b6fc319ff6b0619eb816404e0463973
Barry Lawson <blawson@bates.edu>
2025-09-25 10:42:04 -0400

    Add subtract function to hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 def add(a: int, b: int) -> int:
     return a + b
 
+def subtract(a: int, b: int) -> int:
+    return a - b
+
 def main() -> None:
     print("hello cruel world!")
     print(add(2,3))
